6f6575627db0f9a7016ca88f0745b0f0d267f9dd
Tanguy Benard <t-benard@outlook.fr>
2020-11-02 13:36:12 +0100

    Add interface

--- projet-web-mobile/src/App.vue
+++ projet-web-mobile/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Dashboard from './views/Dashboard.vue'
+  export default {
+    name: 'App',
+    view: {
+      Dashboard
+    }
   }
-}
 </script>
 
 <style>
@@ -23,6 +21,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
