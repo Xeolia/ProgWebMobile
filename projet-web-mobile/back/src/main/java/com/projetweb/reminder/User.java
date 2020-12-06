ee8acf4b207f03a62072b895b9ffdb3380ec0138
Xeolia <alexia.serrierb@gmail.com>
2020-12-06 15:13:02 +0100

    add user propeties

--- projet-web-mobile/back/src/main/java/com/projetweb/reminder/User.java
+++ projet-web-mobile/back/src/main/java/com/projetweb/reminder/User.java
@@ -1,4 +1,65 @@
 package com.projetweb.reminder;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.*;
+
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
 public class User {
+
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+
+
+    public void setEmail(String email) { this.email = email; }
+
+
+
+
+    public String getEmail() { return email; }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
